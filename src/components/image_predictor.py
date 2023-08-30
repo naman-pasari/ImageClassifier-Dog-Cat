43504e6a367f8af6042d3a04cb748c5aa78e11e8
Neo <pasari1002@gmail.com>
2023-08-30 19:31:37 +0530

    Updating Image Predictor

--- src/components/image_predictor.py
+++ src/components/image_predictor.py
@@ -5,8 +5,9 @@ try:
     def predict(image_path):
         image= cv2.imread(image_path)
         resize = tf.image.resize(image, (256,256))
-        new_model = load_model(os.path.join("imageclassifier","imageclassifier.h5"))
+        new_model = load_model(os.path.join(os.getcwd(),'notebook',"models","imageclassifier.h5"))
         prediction=new_model.predict(np.expand_dims(resize/255, 0))
+        print(prediction)
         if prediction > 0.5: 
             print(f'Predicted class is Dog')
         else:
@@ -19,5 +20,5 @@ except Exception as e:
 
 if __name__=="__main__":
     #Giving a custom image path
-    image_path=os.path.join(os.getcwd(),'notebook','images','test_dog1.jpg')
+    image_path=os.path.join(os.getcwd(),'notebook','images','test_dog.jpg')
     predict(image_path=image_path)
